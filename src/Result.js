a2d4d1df5a9e5e853275a67feaddd424e1bef4d0
Kate Haskell <fuzzcat@bactroid.net>
2017-05-28 15:47:27 -0400

    Updated to latest checkprime and handled new null returns

--- src/Result.js
+++ src/Result.js
@@ -3,21 +3,22 @@ import './Result.css'
 import Factors from './Factors'
 import {isPrime} from 'checkprime'
 
-const getText = (num, prime) =>
-  prime
+const getText = (num, prime) => {
+  return prime
     ? (<p>{num} is prime.</p>)
-    : Number.isInteger(num)
+    : Number.isInteger(num) && prime !== null
       ? (<p>{num} is not prime</p>)
       : null
+}
 
 const Result = ({num}) => {
-  const primeResult = Number.isInteger(num)
+  const primeResult = Number.isInteger(num) && num > 1
     ? isPrime(num)
     : null
   return (
     <div id='result'>
       {getText(num, primeResult)}
-      <Factors display={!primeResult && num} num={num} />
+      <Factors display={primeResult === false && primeResult !== null && num} num={num} />
     </div>
   )
 }
